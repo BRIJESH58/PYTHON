75229f12cc2fedd7feca06f9c50e6582b5fe0ff4
BRIJESH58 <96729945+BRIJESH58@users.noreply.github.com>
2022-01-22 15:22:22 +0530

    Delete unpacking3.py

--- unpacking3.py
+++
@@ -1,8 +0,0 @@
-countries = ['germany', 'france', 'belgium', 'sweden', 'denmark', 'finland', 'norway', 'iceland', 'estonia']
-gr, fr, bg, sw, *scandic, es = countries
-print(gr)
-print(fr)
-print(bg)
-print(sw)
-print(scandic)
-print(es)
