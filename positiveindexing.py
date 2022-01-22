d74f127685bcf19146f028c35b7fe609641d33b9
BRIJESH58 <96729945+BRIJESH58@users.noreply.github.com>
2022-01-22 15:17:37 +0530

    Delete positiveindexing.py

--- positiveindexing.py
+++
@@ -1,9 +0,0 @@
-fruits = ['banana', 'orange', 'mango', 'lemon']
-first_fruit = fruits[0]
-print(first_fruit)
-second_fruit = fruits[1]
-print(second_fruit)
-third_fruit = fruits[2]
-print(third_fruit)
-four_fruit = fruits[3]
-print(four_fruit)
