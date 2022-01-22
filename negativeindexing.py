0b18e7d68f652efecdbd37f4096d05b1f9e4814a
BRIJESH58 <96729945+BRIJESH58@users.noreply.github.com>
2022-01-22 15:17:16 +0530

    Delete negativeindexing.py

--- negativeindexing.py
+++
@@ -1,9 +0,0 @@
-fruits = ['banana', 'orange', 'mango', 'lemon']
-first_fruit = fruits[-4]
-second_fruit = fruits[-3]
-third_fruit = fruits[-2]
-four_fruit = fruits[-1]
-print(first_fruit)
-print(second_fruit)
-print(third_fruit)
-print(four_fruit)
